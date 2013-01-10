0f7d895628c6eb0da9e48598c36f04f8305a80d7
Johann Rocholl <johann@rocholl.net>
2013-01-10 00:35:05 -0800

    MakerGear hotend is slightly longer than J-Head.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -56,7 +56,7 @@
 // Make delta curves from many straight lines (linear interpolation).
 // This is a trade-off between visible corners (not enough segments)
 // and processor overload (too many expensive sqrt calls).
-#define DELTA_SEGMENTS_PER_SECOND 200
+#define DELTA_SEGMENTS_PER_SECOND 100
 
 // Center-to-center distance of the holes in the diagonal push rods.
 #define DELTA_DIAGONAL_ROD 240.0 // mm
@@ -297,7 +297,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 // For deltabots this means top and center of the cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 222  // Distance between nozzle and print surface after homing.
+#define MANUAL_Z_HOME_POS 217  // Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
